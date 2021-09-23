95301aaacfb0ff9d94ffd1f7a904a4d1ca4eef66
antti-karkkainen <antti.karkkainen@esedulainen.fi>
2021-09-23 10:53:10 +0300

    Metodeita...

--- Kayttoliittyma.java
+++ Kayttoliittyma.java
@@ -1,1 +1,35 @@
+import java.util.Scanner;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
+public class Kayttoliittyma {
+
+    private ArrayList<String> sanat;
+    private String sana;
+    
+
+
+    public Kayttoliittyma() {
+        this.sanat = new ArrayList<>();
+    }
+
+    public void lueTiedosto() {
+        try (Scanner lukija = new Scanner(Paths.get("tiedot.txt"))) {
+
+            while (lukija.hasNextLine()) {
+                String rivi = lukija.nextLine();
+                this.sanat.add(rivi);                             
+            }
+
+        }  catch (Exception e) {
+            System.out.println("Virhe: tiedostoa ei löydy!" + e.getMessage());
+        }              
+    }
+
+    public String randomSana() {
+        return this.sanat.get((int) Math.random() * this.sanat.size());
+    }
+
+
+
+}
