08abdac28861e5e8a123fff7c3bdf6a41135c23a
Mikko Ralli <mikko.ralli@esedulainen.fi>
2021-09-24 09:18:49 +0300

    hirsipuun rakentaminen josVaarin-metodiin

--- Kayttoliittyma.java
+++ Kayttoliittyma.java
@@ -62,6 +62,97 @@ public class Kayttoliittyma {
     }
 
     public void josVaarin() {
-     
+        if ( vaarin == 1) {
+            System.out.println("Väärä kirjain, sinulla on 6 yritystä jäljellä.");
+            System.out.println(" -------");
+            System.out.println(" |     |");
+            System.out.println("       |");
+            System.out.println("       |");
+            System.out.println("       |");
+            System.out.println("       |");
+            System.out.println("----------");
+        }
+
+        if (vaarin == 2) {
+            System.out.println("Väärä kirjain, sinulla on 5 yritystä jäljellä.");
+            System.out.println(" -------");
+            System.out.println(" |     |");
+            System.out.println(" O     |");
+            System.out.println("       |");
+            System.out.println("       |");
+            System.out.println("       |");
+            System.out.println("       |");
+            System.out.println("----------");
+        }
+
+        if (vaarin == 3) {
+            System.out.println("Väärä kirjain, sinulla on 4 yritystä jäljellä.");
+            System.out.println(" -------");
+            System.out.println(" |     |");
+            System.out.println(" O     |");
+            System.out.println(" |     |");
+            System.out.println(" |     |");
+            System.out.println("       |");
+            System.out.println("       |");
+            System.out.println("       |");
+            System.out.println("       |");
+            System.out.println("----------");
+        }
+
+        if (vaarin == 4) {
+            System.out.println("Väärä kirjain, sinulla on 3 yritystä jäljellä.");
+            System.out.println("  -------");
+            System.out.println("  |     |");
+            System.out.println("  O     |");
+            System.out.println(" \|     |");
+            System.out.println("  |     |");
+            System.out.println("        |");
+            System.out.println("        |");
+            System.out.println("        |");
+            System.out.println("        |");
+            System.out.println(" ----------");
+        }
+
+        if (vaarin == 5) {
+            System.out.println("Väärä kirjain, sinulla on 2 yritystä jäljellä.");
+            System.out.println("  -------");
+            System.out.println("  |     |");
+            System.out.println("  O     |");
+            System.out.println(" \|/    |");
+            System.out.println("  |     |");
+            System.out.println("        |");
+            System.out.println("        |");
+            System.out.println("        |");
+            System.out.println("        |");
+            System.out.println(" ----------");
+        }
+
+        if (vaarin == 6) {
+            System.out.println("Väärä kirjain, sinulla on 1 yritys jäljellä.");
+            System.out.println("  -------");
+            System.out.println("  |     |");
+            System.out.println("  O     |");
+            System.out.println(" \|/    |");
+            System.out.println("  |     |");
+            System.out.println(" /      |");
+            System.out.println("        |");
+            System.out.println("        |");
+            System.out.println("        |");
+            System.out.println(" ----------");
+        }
+
+        if (vaarin == 7) {
+            System.out.println("Väärä kirjain, hävisit pelin!");
+            System.out.println("  -------");
+            System.out.println("  |     |");
+            System.out.println("  O     |");
+            System.out.println(" \|/    |");
+            System.out.println("  |     |");
+            System.out.println(" / \    |");
+            System.out.println("        |");
+            System.out.println("        |");
+            System.out.println("        |");
+            System.out.println(" ----------");
+        }
     }
 }
