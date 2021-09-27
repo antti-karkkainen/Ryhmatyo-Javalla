3b63b62e967804006352636d74ccd0da85ceefaa
Mikko Ralli <mikko.ralli@esedulainen.fi>
2021-09-27 09:35:50 +0300

    vanhoja koodeja

--- Testihirsipuu.java
+++ Testihirsipuu.java
@@ -2,15 +2,7 @@ public class Testihirsipuu {
     
     public int vaarin = 0; 
 
-    out.append("The word: ");
-                for (int i = 0; i < word.length(); ++i) {
-                    if (visible[i]) {
-                        out.print(word.charAt(i));
-                    } else {
-                        out.print("?");
-                    }
-                }
-                out.append("\n\n");
+    
 
     public static void alaviiva(String arvaus) {
 		String alaviiva = "";
