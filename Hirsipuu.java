455c10418653ffba9d18f3eb7adbe6480d29e731
antti-karkkainen <antti.karkkainen@esedulainen.fi>
2021-10-04 14:00:23 +0300

    Aloitettu arvaaKirjain() metodia..

--- Hirsipuu.java
+++ Hirsipuu.java
@@ -1,1 +1,22 @@
+import java.util.ArrayList;
 
+
+public class Hirsipuu {
+
+    Kayttoliittyma kayttoliittyma = new Kayttoliittyma();
+
+    private String sana;
+    
+    public Hirsipuu() {
+        this.sana = kayttoliittyma.randomSana();
+    }
+
+    public void arvaaKirjain(String arvaus) {
+        arvaus = arvaus.toUpperCase();
+        char arvausKirjaimena = arvaus.charAt(0);
+        String alaviivat = this.sana.replaceAll("[A-Z]", "_ "); // Voisikohan alaviivojen piirtämisen toteuttaa näin? Mutta miten vaihtaa arvatun kirjaimen alaviivojen kohdalle...? 
+                                                                //Ehkä loop joka tarkkailee sanan indekseissä olevia kirjaimia ja vaihtaa tietyn indeksin kirjaimen viivaksi, jos siinä indeksissä löytyy arvattu kirjain. Eli loop ja if lausekkeita. Kokeilen huomenna.
+        
+    }
+
+}
